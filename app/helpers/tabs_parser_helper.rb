57faecf769f0109a3ebea2108a6639c78f7c9469
avinashak47 <avinash.ak@gmail.com>
2013-02-25 00:47:28 -0800

    Initial commit. Rals 3.2.12 setup with basic parser, test for basic parser done through interactive shell.first todo add test for tabs parser

---
+++ app/helpers/tabs_parser_helper.rb
@@ -0,0 +1,37 @@
+class TabsParserHelper
+	def validate( str )
+		#refactor into strategy if required
+		#match first char is | or  -
+		#either -, 1|2digit , 1|2digit<h/p/b/s>2digit
+		if(!str.is_a?(String)) 
+			return false
+		end
+
+		if (str.length==0)
+			 return false
+		end
+		
+		match_first_result = false
+		match_rest_result = false
+
+		match_first_char = /^[|,\-,\d]/.match( str )
+		
+		if (!match_first_char.nil?)
+			match_first_result = match_first_char[0] == str[0]
+		end
+
+		if (str.length==1 || !match_first_result )
+			return match_first_result
+		end
+
+		match_rest = /((?>\-\d{1,2}[hpbs]{1}\d{1,2})|(?>\-\d{1,2})|(?>\-))+/.match(str,1)
+		
+		if (!match_rest.nil?)
+			match_rest_result = match_rest.to_s == str.slice(1..-1) 
+		end
+
+		return (match_first_result && match_rest_result)
+		
+	end
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mytabsfall::Application.config.secret_token = '643c8a3f7edf4c2829547a40164689cf5f57062ce380cecaca91f08cb507e4b68b4f62f44d9703f52082d4f52665d277e40b25f97cd3eac789d35e7ac100315d'
